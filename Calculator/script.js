1cfd1a898738a23aa6e3a9a5404ee6c374d5e40c
Selvaraj <selva.javascript@gmail.com>
2023-04-26 18:23:26 +0530

    v1.0

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,5 +1,7 @@
-const display =document.querySelector(".display");
-const buttons = documnet.querySelectorAll("button");
+const display = document.querySelector(".display");
+
+const buttons = document.querySelectorAll("button");
+
 const specialChars = ["%","*","/","-","+","="];
 let output = "";
 
@@ -13,20 +15,20 @@ const calculate = (btnValue) =>{
         output = "";
     }else if(btnValue === "DEL"){
         //If DEL button is clicked remove the last character from the output
-        output = output.toString().slice(1,-1);
+        output = output.toString().slice(0,-1);
     }else{
         //If output is empty and button is specialChars then return
-        if(output === "" && specialChars.includes(btnValue))
-        return;
+        if(output === "" && specialChars.includes(btnValue))return;
         output += btnValue;
     }
   display.value = output;  
 };
 
 //Add event listener to buttons, call calculate() on click.
-
-buttons.foreach((button)=>{
+buttons.forEach((button)=>{
     //Button click listerner calls calculate() with dataset value as arguments.
     button.addEventListener("click", (e)=> calculate(e.target.dataset.value));
 });
+
+
  
